4fc8490c91fc4197e69570baa0059b149c8ec226
abiraj-rajendran <abiraj.rajendran@zoho.com>
2021-11-02 13:46:25 +0530

    mobile methods

--- app.py
+++ app.py
@@ -1,12 +1,14 @@
 import firebase_admin
 from firebase_admin import credentials, firestore
-from flask import Flask, render_template , request
-
+from flask import Flask, render_template , request,jsonify
+import psycopg2
 cred = credentials.Certificate("service_accountkey.json")
 firebase_admin.initialize_app(cred)
 
 db = firestore.client()
 app = Flask(__name__)
+
+
 
 def addpatientdata(data, id1):
     db.collection('patient').document(id1).set(data)
@@ -247,6 +249,98 @@ def updatepatientdata():
         addpatientdata(data, id1)
 
     return render_template("editpatient.html")
+
+#api for postgres covitrack
+host = "agada-s1.postgres.database.azure.com"
+dbname = "postgres"
+user = "agada@agada-s1"
+password = "Password123$"
+sslmode = "require"
+
+def getCon():
+    conn_string = "host={0} user={1} dbname={2} password={3} sslmode={4}".format(host, user, dbname, password, sslmode)
+    conn = psycopg2.connect(conn_string)
+    print("Connection established")
+    cursor = conn.cursor()
+    return cursor
+
+def addPatient(email,phone):
+    cursor = getCon()
+    cursor.execute("insert into users values(%s,%s)",(email,phone))
+    cursor.close()
+
+def addEmergency(email,date):
+    cursor = getCon()
+    cursor.execute("insert into emergency values(%s,%s)",(email,date))
+    cursor.close()
+
+def addDailyTest(email,score,date):
+    cursor = getCon()
+    cursor.execute("insert into daily values(%s,%s,%s)",(email,score,date))
+    cursor.close()
+
+def addPrescription(email,morning,afternoon,evening):
+    cursor = getCon()
+    cursor.execute("insert into prescription values(%s,%s,%s,%s)",(email,morning,afternoon,evening))
+    cursor.close()
+
+
+def checkUserInDB(email,phone):
+    cursor = getCon()
+    cursor.execute("select * from users where email=%s and mobile=%s",(email,phone))
+    data = cursor.fetchall()
+    cursor.close()
+    return True if len(data)>0 else False
+
+@app.route('/checkUser',methods=['POST'])
+def checkUser():
+    data=request.get_json()
+    email=data['email']
+    mobile=data['mobile']
+    return jsonify({"status":checkUserInDB(email,mobile)})
+
+
+
+@app.route('/addDailyScore',methods=['POST'])
+def addDailyScore():
+    data=request.get_json()
+    email=data['email']
+    score=data['score']
+    date=data['date']
+    addDailyTest(email,score,date)
+    return jsonify({"status":"success"})
+
+@app.route('/addEmergency',methods=['POST'])
+def addEmergencyUser():
+    data=request.get_json()
+    email=data['email']
+    date=data['date']
+    addEmergency(email,date)
+    return jsonify({"status":"success"})
+
+
+@app.route('/getPrescription',methods=['GET'])
+def getPrescription():
+    data=request.get_json()
+    email=data['email']
+    cursor = getCon()
+    print(email)
+    cursor.execute("select * from prescription where id='"+email+"'")
+    users = cursor.fetchall()
+    result={}
+    patients = []
+   
+    for user in users:
+        patient = {
+            'id': user[0],
+            'morning': user[1],
+            'afternoon': user[2],
+            'evening':user[3],
+        }
+        patients.append(patient)
+    cursor.close()
+    result['pres'] = patients
+    return jsonify(result)
 
 if __name__ == '__main__':
     app.run(debug=True)
